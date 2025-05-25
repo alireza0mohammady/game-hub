46b599da28a0bfdeb823e05d411da673d49e2bd3
alireza0mohammadi <mr.afg00@gmail.com>
2025-05-25 20:40:26 +0330

    create Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,58 @@
+import { SiIos } from "react-icons/si";
+import { BsNintendoSwitch } from "react-icons/bs";
+import {
+  FaPlaystation,
+  FaXbox,
+  FaWindows,
+  FaLinux,
+  FaApple,
+  FaAndroid,
+} from "react-icons/fa6";
+import getCropedImageUrl from "../services/img-url";
+
+const Card = (props) => {
+  const { name, img, platforms, meta } = props;
+
+  const platformsLogos = platforms.map((item) => {
+    if (item.platform.slug == "pc") return <FaWindows key={item.platform.id} />;
+    if (item.platform.slug == "playstation")
+      return <FaPlaystation key={item.platform.id} />;
+    if (item.platform.slug == "xbox") return <FaXbox key={item.platform.id} />;
+    if (item.platform.slug == "linux")
+      return <FaLinux key={item.platform.id} />;
+    if (item.platform.slug == "nintendo")
+      return <BsNintendoSwitch key={item.platform.id} />;
+    if (item.platform.slug == "ios") return <SiIos key={item.platform.id} />;
+    if (item.platform.slug == "mac") return <FaApple key={item.platform.id} />;
+    if (item.platform.slug == "android")
+      return <FaAndroid key={item.platform.id} />;
+  });
+
+  function metaColor() {
+    if (!meta) return "";
+    if (meta >= 80) return "text-green-500 border-green-500/40";
+    if (meta >= 60) return "text-yellow-500 border-yellow-500/40";
+    return "text-red-500 border-red-500/40";
+  }
+
+  return (
+    <div>
+      <section className="dark:bg-dark-secondery shadow-md rounded-xl overflow-hidden flex flex-col gap-3 dark:text-gray-50">
+        <section>
+          <img src={getCropedImageUrl(img)} alt="" />
+        </section>
+        <section className="p-4 flex flex-col gap-1">
+          <div className="flex justify-between items-center gap-2">
+            <div className="flex gap-1">{platformsLogos}</div>
+            <div className={metaColor() + " px-1.5 rounded-sm border"}>
+              {meta}
+            </div>
+          </div>
+          <div className="font-bold text-2xl">{name}</div>
+        </section>
+      </section>
+    </div>
+  );
+};
+
+export default Card;
