871f1e11c580e1feccf506b335f234d60c29e98e
alireza0mohammadi <mr.afg00@gmail.com>
2025-07-08 20:48:26 +0330

    adding sort and platform selector components

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
 import gameApi from "../../services/games-api";
 import GridSection from "./GridSection";
+import GamesGridSkeleton from "./GamesGridSkeleton";
+import PlatformSelector from "./PlatformSelector";
+import SortSelector from "./SortSelector";
 
-const Main = () => {
+const Main = (props) => {
+  const { selectedGenre } = props;
+  const [selectedSortMethod, setSelectedSortMethod] = useState(null);
   const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [gridCols, setGridCols] = useState(null);
+  const [selectedPlatform, setSelectedPlatform] = useState(null);
 
   useEffect(() => {
     const handleResize = () => {
@@ -26,19 +33,30 @@ const Main = () => {
   }, []);
 
   useEffect(() => {
-    const { request, cancel } = gameApi.getGames();
+    setLoading(true);
+    console.log(selectedSortMethod);
+    const { request, cancel } = gameApi.get({
+      ...(selectedSortMethod ? { ordering: selectedSortMethod.id } : {}),
+      ...(selectedGenre ? { genres: selectedGenre } : {}),
+      ...(selectedPlatform
+        ? { parent_platforms: selectedPlatform.id.toString() }
+        : {}),
+    });
     request
       .then((res) => {
+        console.log(res.data);
+        setLoading(false);
         setResult(res.data.results);
       })
       .catch((error) => {
+        setLoading(false);
         if (error.name === "CanceledError") return;
         setError(error);
       });
     return () => {
       cancel();
     };
-  }, []);
+  }, [selectedGenre, selectedPlatform, selectedSortMethod]);
 
   function mainDynamicGridClass() {
     if (gridCols === 1) {
@@ -56,14 +74,30 @@ const Main = () => {
   }
 
   return (
-    <main
-      className={
-        mainDynamicGridClass() +
-        " grid gap-x-2.5 md:gap-x-5 lg:w-6/7 px-2.5 py-2.5 items-start"
-      }
-    >
-      {error ? error.message : ""}
-      {result ? <GridSection result={result} gridCols={gridCols} /> : "no data"}
+    <main className="lg:w-6/7 ">
+      <section>
+        <PlatformSelector
+          selectedPlatform={selectedPlatform}
+          setSelectedPlatform={setSelectedPlatform}
+        />
+        <SortSelector
+          selectedSortMethod={selectedSortMethod}
+          setSelectedSortMethod={setSelectedSortMethod}
+        />
+      </section>
+      <section
+        className={
+          mainDynamicGridClass() +
+          " grid gap-x-2.5 md:gap-x-5 px-2.5 py-2.5 items-start"
+        }
+      >
+        {error ? error.message : ""}
+        {result && !loading ? (
+          <GridSection result={result} gridCols={gridCols} />
+        ) : (
+          <GamesGridSkeleton />
+        )}
+      </section>
     </main>
   );
 };
