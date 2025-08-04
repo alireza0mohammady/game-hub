2560b5e4b3cb7e675f0c5e3b3b0936ca2dd55461
alireza0mohammadi <mr.afg00@gmail.com>
2025-08-04 21:13:31 +0330

    adding gameQuery(games end point params) state and set functions for it

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import Aside from "./components/Aside/Aside";
 import NavBar from "./components/NavBar/NavBar";
 import Main from "./components/Main/Main";
+import { useState } from "react";
 
 function App() {
+  const [gameQuery, setGameQuery] = useState({
+    genres: null,
+    ordering: null,
+    platforms: null,
+    search: null,
+  });
+  function setSelectedGenre(genre) {
+    setGameQuery({ ...gameQuery, genres: genre });
+  }
+  function setSelectedPlatform(selectedPlatform) {
+    setGameQuery({ ...gameQuery, platforms: selectedPlatform });
+  }
+  function setSortingMethod(method) {
+    setGameQuery({ ...gameQuery, ordering: method });
+  }
+  function setSearch(name) {
+    setGameQuery({ ...gameQuery, search: name });
+  }
   return (
     <>
-      <NavBar></NavBar>
-      <section className="flex flex-col lg:flex-row">
-        <Aside></Aside>
-        <Main></Main>
+      <NavBar gameQuery={gameQuery} setSearch={setSearch}></NavBar>
+      <section className="flex flex-col md:flex-row">
+        <Aside
+          gameQuery={gameQuery}
+          setSelectedGenre={setSelectedGenre}
+        ></Aside>
+        <Main
+          gameQuery={gameQuery}
+          setSelectedPlatform={setSelectedPlatform}
+          setSortingMethod={setSortingMethod}
+        ></Main>
       </section>
     </>
   );
