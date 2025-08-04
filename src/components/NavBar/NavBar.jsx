c4c2ce1aa43d7318b007fb5138f4f6aaf432ca65
alireza0mohammadi <mr.afg00@gmail.com>
2025-08-04 22:14:14 +0330

    adding props to search bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,13 +2,14 @@ import logo from "../../assets/comi-cog.png";
 import DarkModeButton from "./DarkModeButton";
 import SearchBar from "./SearchBar";
 
-const NavBar = () => {
+const NavBar = (props) => {
+  const { gameQuery, setSearch } = props;
   return (
     <nav className="flex gap-6 items-center px-4 py-4">
       <section className="min-w-8">
         <img src={logo} alt="" className="w-8 lg:w-11" />
       </section>
-      <SearchBar></SearchBar>
+      <SearchBar gameQuery={gameQuery} setSearch={setSearch}></SearchBar>
       <section className="">
         <DarkModeButton />
       </section>
